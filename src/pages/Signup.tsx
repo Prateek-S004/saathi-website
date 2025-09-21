213d6a13b89c50e9ae59e8fb8970cec348d6f8e1
Prateek-S004 <psachhu00799@gmail.com>
2025-09-21 23:45:33 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -15,7 +15,7 @@ const Signup: React.FC = () => {
     setError("");
 
     try {
-      await axios.post("https://saathi-website.onrender.com/api/auth/signup", {
+      await axios.post("https://saathi-website-production.up.railway.app/api/auth/signup", {
         username,
         password,
         role,
