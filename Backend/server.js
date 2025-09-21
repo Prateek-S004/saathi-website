2a52878e2dc0526e9f3c6c9e042d8e178f861a7a
Prateek-S004 <psachhu00799@gmail.com>
2025-09-21 22:00:26 +0530

    port change

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ import analyticsRoutes from "./routes/analyticsRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
